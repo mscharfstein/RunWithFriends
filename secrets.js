7955729fa36632396e7d785b252c432d99e1d1cb
Michelle Scharfstein <michelle.scharfstein@gmail.com>
2017-09-21 09:49:38 -0400

    remove secrets

--- secrets.js
+++
@@ -1,3 +0,0 @@
-process.env.GOOGLE_CLIENT_ID = '561218665811-3orot3ocgfo080j5gft6n6rvrm4c1qt8.apps.googleusercontent.com'
-process.env.GOOGLE_CLIENT_SECRET = 'Eos9LkyL2G_baY-GkpdpcsuQ'
-process.env.GOOGLE_CALLBACK = 'auth/google/callback'
